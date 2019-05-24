b7a68fa8441735d33a5045c4b79b2c0b9a319cc7
vvizard <r.haritiana.z@gmail.com>
2019-05-24 12:40:57 +0300

    Correction of refresh_token

--- src/main/java/org/mlp/apps/config/AuthorizacionServerConfiguration.java
+++ src/main/java/org/mlp/apps/config/AuthorizacionServerConfiguration.java
@@ -1,9 +1,12 @@
 package org.mlp.apps.config;
 
+import org.mlp.apps.config.user.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -23,6 +26,11 @@ public class AuthorizacionServerConfiguration  extends AuthorizationServerConfig
 	  
 	@Autowired
 	private AppConfig appConfig;
+	
+	@Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
 
 	
 	@Override
@@ -33,7 +41,7 @@ public class AuthorizacionServerConfiguration  extends AuthorizationServerConfig
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 	    endpoints
-	            .authenticationManager(authenticationManager)	        
+	            .authenticationManager(authenticationManager).userDetailsService(userDetailsService())	        
 	            .tokenStore(tokenStore);
 	}
 	 
