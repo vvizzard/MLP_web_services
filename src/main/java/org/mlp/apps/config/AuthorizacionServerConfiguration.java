4b60a85de24725bdd41605c194dcf376c562e2c7
vvizzard <r.haritiana.z@gmail.com>
2019-06-17 12:16:50 +0300

    Revert "Correction of refresh_token"
    This reverts commit b7a68fa8441735d33a5045c4b79b2c0b9a319cc7.

--- src/main/java/org/mlp/apps/config/AuthorizacionServerConfiguration.java
+++ src/main/java/org/mlp/apps/config/AuthorizacionServerConfiguration.java
@@ -1,12 +1,9 @@
 package org.mlp.apps.config;
 
-import org.mlp.apps.config.user.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -26,11 +23,6 @@ public class AuthorizacionServerConfiguration  extends AuthorizationServerConfig
 	  
 	@Autowired
 	private AppConfig appConfig;
-	
-	@Bean
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsServiceImpl();
-    }
 
 	
 	@Override
@@ -41,7 +33,7 @@ public class AuthorizacionServerConfiguration  extends AuthorizationServerConfig
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 	    endpoints
-	            .authenticationManager(authenticationManager).userDetailsService(userDetailsService())	        
+	            .authenticationManager(authenticationManager)	        
 	            .tokenStore(tokenStore);
 	}
 	 
